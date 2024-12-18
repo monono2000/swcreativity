9c90c5fd6345436abdb714ac0e005bf6f7b6ffb2
monono <82625749+monono2000@users.noreply.github.com>
2024-12-18 12:46:47 +0900

    Update size.sql

--- size.sql
+++ size.sql
@@ -4,22 +4,20 @@ CREATE TRIGGER after_bodyinfo_insert
 AFTER INSERT ON BodyInfo
 FOR EACH ROW
 BEGIN
-    IF NEW.height_cm <= 160 AND NEW.weight_kg <= 55 THEN
+    -- 상의 사이즈 설정
+    IF NEW.height_cm = 160 AND NEW.weight_kg = 55 THEN
         UPDATE BodyInfo SET top_size = 'S', outer_size = 'S' WHERE body_id = NEW.body_id;
-    ELSEIF NEW.height_cm <= 170 AND NEW.weight_kg <= 70 THEN
+    ELSEIF NEW.height_cm = 165 AND NEW.weight_kg = 62 THEN
         UPDATE BodyInfo SET top_size = 'M', outer_size = 'M' WHERE body_id = NEW.body_id;
-    ELSE
+    ELSEIF NEW.height_cm = 170 AND NEW.weight_kg = 67 THEN
         UPDATE BodyInfo SET top_size = 'L', outer_size = 'L' WHERE body_id = NEW.body_id;
-    END IF;
-
-    IF NEW.waist_cm <= 70 THEN
-        UPDATE BodyInfo SET bottom_size = 28.0 WHERE body_id = NEW.body_id;
-    ELSEIF NEW.waist_cm <= 80 THEN
-        UPDATE BodyInfo SET bottom_size = 30.0 WHERE body_id = NEW.body_id;
-    ELSEIF NEW.waist_cm <= 90 THEN
-        UPDATE BodyInfo SET bottom_size = 32.0 WHERE body_id = NEW.body_id;
+    ELSEIF NEW.height_cm = 175 AND NEW.weight_kg = 73 THEN
+        UPDATE BodyInfo SET top_size = 'XL', outer_size = 'XL' WHERE body_id = NEW.body_id;
+    ELSEIF NEW.height_cm = 180 AND NEW.weight_kg = 80 THEN
+        UPDATE BodyInfo SET top_size = 'XXL', outer_size = 'XXL' WHERE body_id = NEW.body_id;
     ELSE
-        UPDATE BodyInfo SET bottom_size = 34.0 WHERE body_id = NEW.body_id;
+        -- 기본값 설정
+        UPDATE BodyInfo SET top_size = 'Unknown', outer_size = 'Unknown' WHERE body_id = NEW.body_id;
     END IF;
 END;
 //
